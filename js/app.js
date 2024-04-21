f420719465635c9840d078188f8648ab00f3189b
rahmathullahmasum <masum22nd@gmail.com>
2024-04-22 01:55:14 +0600

    login done

--- js/app.js
+++ js/app.js
@@ -88,6 +88,7 @@
     After Load All Content Add a Class
     -------------------------------------*/
     window.onload = addNewClass();
+   
 
     function addNewClass() {
         $('.fxt-template-animation').imagesLoaded().done(function(instance) {
