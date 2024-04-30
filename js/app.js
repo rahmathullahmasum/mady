891b7c70d8c0177678df562fb92b7f5423a9b456
rahmathullahmasum <masum22nd@gmail.com>
2024-05-01 02:12:49 +0600

    typo

--- js/app.js
+++ js/app.js
@@ -99,8 +99,18 @@
     /*-------------------------------------
     Toggle Class
     -------------------------------------*/
-    $(".toggle-password").on('click', function() {
-        $(this).toggleClass("fa-eye fa-eye-slash");
+    // $(".toggle-password").on('click', function() {
+    //     $(this).toggleClass("fa-eye fa-eye-slash");
+    //     var input = $($(this).attr("toggle"));
+    //     if (input.attr("type") == "password") {
+    //         input.attr("type", "text");
+    //     } else {
+    //         input.attr("type", "password");
+    //     }
+    // });
+
+    $(".toggle-password").on('click', function () {
+        $(this).toggleClass("fa-eye");
         var input = $($(this).attr("toggle"));
         if (input.attr("type") == "password") {
             input.attr("type", "text");
